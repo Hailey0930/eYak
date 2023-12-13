cc739ecd2ef4afa07cdcc699cff3ac08f0058d73
HyerinJeon <hyerin0930@gmail.com>
2023-12-13 11:31:07 +0900

    :lipstick: Add Layout
    Add Layout with header

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+import styled from "styled-components";
+
+type ILayoutProps = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: ILayoutProps) {
+  return (
+    <LayoutContainer>
+      <Header />
+      {children}
+    </LayoutContainer>
+  );
+}
+
+const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+`;
