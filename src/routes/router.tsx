ff375943354d2f097b02fa1fc98f821e8b7a02ca
HyerinJeon <hyerin0930@gmail.com>
2023-12-14 11:00:00 +0900

    :rocket: Run build

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,21 +2,6 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
 import Layout from "../components/layout";
 
-<<<<<<< HEAD
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <Layout>
-          <Main />
-        </Layout>
-      ),
-    },
-  ],
-  { basename: process.env.REACT_APP_BASE_URL },
-);
-=======
 export const AppRouter = () => {
   return (
     <Router>
@@ -33,4 +18,3 @@ export const AppRouter = () => {
     </Router>
   );
 };
->>>>>>> develop
